7e0a211d8b7f22791d755934d48504a70f4a58be
LM088 <69414981+LM088@users.noreply.github.com>
2025-08-22 13:57:42 +0000

    Add shaders and BOs to draw quadilateral

--- src/quad.cpp
+++ src/quad.cpp
@@ -2,6 +2,21 @@
 #include <glad.h>
 #include <GLFW/glfw3.h>
 
+ // Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.0f, 1.0f, 1.0f);\n"
+"}\n\0";
 
 // Prototypes 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -14,6 +29,21 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLfloat vertices[] = 
+    {
+        0.5f, 0.5f, 0.0f,   //top right    -0
+        -0.5f, 0.5f, 0.0f,  //top left     -1
+        -0.5f, -0.5f, 0.0f, //bottom left  -2
+        0.5f, -0.5f, 0.0f   //bottom right -3 
+    };
+
+    // Make sure to start from 0! 
+    GLuint indices[] = 
+    {
+        0, 3, 1,
+        3, 2, 1
+    };
 
     GLFWwindow* window = glfwCreateWindow(900,900,"Hello-Quad",NULL,NULL);
     if (window == NULL)
@@ -28,14 +58,70 @@ int main()
     gladLoadGL();
     glViewport(0,0,900,900);
 
+    // Create vertex and fragment shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); 
+    glCompileShader(vertexShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL );
+    glCompileShader(fragmentShader);
+
+    // Creating shader program 
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram); 
+
+    //Delete vertex and fragment shaders since they're linked now 
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //Creating buffers 
+    GLuint VAO;
+    GLuint VBO; 
+    GLuint EBO; 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO); 
+
+    // Bind buffers
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices , GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Telling OpenGL how to read buffer data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbinding 
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
         glClearColor(0.63f, 0.52f, 0.74f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
